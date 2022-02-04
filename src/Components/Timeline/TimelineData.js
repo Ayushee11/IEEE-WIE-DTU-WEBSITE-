5a88bf0f6581b19c20188d8ef22b7e82ff3f25e8
Ayushee Agarwal <ayusheeagarwal11@gmail.com>
2022-02-04 09:56:58 +0530

    wiewebsite

---
+++ src/Components/Timeline/TimelineData.js
@@ -0,0 +1,31 @@
+import google from "./images/google.png";
+import jpm from "./images/jpm.png";
+import deshaw from "./images/deshaw.png";
+
+export const data = [
+  {
+    heading: "Google Genration Scholarship",
+    description: " Women students(pursuing computer)if selected will receive $1,000 USD as Scholarship",
+    img: google ,
+    date: " Dec 17- Jan 4",
+  },
+  {
+    heading: " DeShaw",
+    description:" Mentorshing and Opputunity to work on tech projects",
+    img: deshaw,
+    date: "30 August",
+  },
+  {
+    heading: "STEP",
+    description:"12 week internship for women",
+    img: google,
+    date: "Multiple Dates",
+  },
+  {
+    heading: "JPMorgan Quant Mentorship",
+    description:"Opportunity to build skills required for a successful career in Quantitative Finance.",
+    img: jpm,
+    date: "16 Feb",
+  },
+ 
+];
